132155805e5891b1dfb2594b8399f7aba346b5b4
LAI, JIA-YING <41336123@nfu.edu.tw>
2025-11-17 16:20:56 +0800

    11/17

--- tcpfilesender.cpp
+++ tcpfilesender.cpp
@@ -2,6 +2,29 @@
 
 TCPFileSender::TCPFileSender(QWidget *parent)
     : QDialog(parent)
-{}
+{
+    loadSize=1024*4;
+    totalBytes=0;
+    bytesToWrite=0;
+    bytesWritten=0;
+    ClientProgressBar = new QProgressBar;
+    clientStatusLabel = new QLabel(tr("客戶端就緒"));
+    startButton = new QPushButton(tr("開始"));
+    quitButton = new QPushButton(tr("=退出"));
+    openButton = new QPushButton(tr("開檔"));
+    startButton->setEnabled(false);
+    buttonBox= new QDialogButtonBox;
+    buttonBox->addButton(startButton,QDialogButtonBox::ActionRole);
+    buttonBox->addButton(openButton,QDialogButtonBox::ActionRole);
+    buttonBox->addButton(quitButton,QDialogButtonBox::RejectRole);
+}
 
 TCPFileSender::~TCPFileSender() {}
+
+void TCPFileSender::start() {}
+
+void TCPFileSender::startTransfer() {}
+
+void TCPFileSender::updateClientProgress(qint64 numBytes) {}
+
+void TCPFileSender::openFile() {}
--- tcpfilesender.h
+++ tcpfilesender.h
@@ -2,6 +2,8 @@
 #define TCPFILESENDER_H
 
 #include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
 
 class TCPFileSender : public QDialog
 {
@@ -10,5 +12,26 @@ class TCPFileSender : public QDialog
 public:
     TCPFileSender(QWidget *parent = nullptr);
     ~TCPFileSender();
+public slots:
+    void start();
+    void startTransfer();
+    void updateClientProgress(qint64 numBytes);
+    void openFile();
+private:
+    QProgressBar *ClientProgressBar;
+    QLabel *clientStatusLabel;
+    QPushButton *startButton;
+    QPushButton *quitButton;
+    QPushButton *openButton;
+    QDialogButtonBox *buttonBox;
+    QTcpSocket tcpClient;
+
+    qint64 totalBytes;
+    qint64 bytesWritten;
+    qint64 bytesToWrite;
+    qint64 loadSize;
+    QString fileName;
+    QFile *localFile;
+    QByteArray outBlock;
 };
 #endif // TCPFILESENDER_H
